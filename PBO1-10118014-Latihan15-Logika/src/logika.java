235f87d89bf6059b1956f74309a27863eb3becb5
JOVIDHARIS11 <55835564+JOVIDHARIS11@users.noreply.github.com>
2019-09-26 18:10:14 +0700

    Add files via upload

---
+++ PBO1-10118014-Latihan15-Logika/src/logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : JOVI D HARIS
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Penggunaan operator logika
+ */
+public class logika {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b) = " + !(a &&b));
+    }
+}
