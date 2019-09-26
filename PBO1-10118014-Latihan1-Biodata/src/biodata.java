534021b9ab02ca7f6b46598c0f0a8a72be642ebf
JOVIDHARIS11 <55835564+JOVIDHARIS11@users.noreply.github.com>
2019-09-26 17:59:31 +0700

    Add files via upload

---
+++ PBO1-10118014-Latihan1-Biodata/src/biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan biodata
+ */
+public class biodata {
+    
+    public static void main(String[] args){
+        System.out.println("Biodat" +"\n"+ "----------------------------");
+        System.out.println("Nama\t\t:Alfian Soasiu");
+        System.out.println("Alamat\t\t:Tubagus Ismail");
+        System.out.println("Tempat Lahir\t:Ambon");
+        System.out.println("Tanggal Lahir\t:03 Desember 2000");
+        System.out.println("----------------------------");
+        System.out.println("Hobi\t\t:Nonton Youtube");
+        System.out.println("Makes\t\t:Ikan Goreng");
+        System.out.println("Mikes\t\t:Air Putih");
+    }
+   
+}
